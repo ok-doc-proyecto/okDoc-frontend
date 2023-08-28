82d027d48858ef073bf68e369614aa2e0a51a09a
tomasmonicat <tomasmonicat@gmail.com>
2023-08-28 16:16:49 +0200

    Hecha la conexión con django API /all-docs/
    Se modificaron main.js, agregando axios, y HomeView.vue para recibir de
    la API del backend.

--- src/main.js
+++ src/main.js
@@ -5,12 +5,13 @@ import router from './router'
 import { FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core';
 import {fas} from '@fortawesome/free-solid-svg-icons'
+import axios from 'axios'
 
 
 library.add(fas);
 
-
+axios.defaults.baseURL = '//localhost:8000';
 const app = createApp(App)
 app.component('fa',FontAwesomeIcon)
-app.use(router)
+app.use(router, axios)
 app.mount('#app')
